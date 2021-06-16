0931b7652ddf95e8fecd65b962eaf669f5a273e4
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-06-16 16:36:35 +0530

    Added : Employee_Wages_UC

---
+++ employee_UseCases/EmpWageBuilderUC1.java
@@ -0,0 +1,16 @@
+package employee_UseCases;
+
+public class EmpWageBuilderUC1 {
+	public static void main(String[] args) {
+		// Constants
+		int IS_FULL_TIME = 1;
+		// Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("EMPLOYEE IS PRESENT");
+		else
+			System.out.println("EMPLOYEE IS ABSENT");
+
+	}
+
+}
